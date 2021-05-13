90b15245a36ba6157e37e290fdd355c970a3585f
negin1 <negin.jalalian@gmail.com>
2021-05-13 20:50:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl} from './spotify';
+import SpotifyWebbApi from 'spotify-web-api-js'
+import Player from './Player'
+
+const spotify = new SpotifyWebbApi();
 
 function App() {
+
+  const [token, setToken] = useState();
+
+  //Run code based on a given condition 
+  useEffect(() =>{
+    const hash  = getTokenFromUrl();
+    window.location.hash ="";
+    const _token = hash.access_token;
+
+    if(_token){
+       setToken(_token);
+
+       spotify.setAccessToken(_token);
+
+       spotify.getMe().then(user =>
+        console.log('x', user))
+    }
+    
+
+  console.log("hej", token);
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      token ? (
+        <Player />
+      ) :(
+         <Login />
+      )
+    }
+    
+
     </div>
   );
 }
